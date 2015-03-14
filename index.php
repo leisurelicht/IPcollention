9b42e52b2b98df83eab57241d02d51a4cd5c8385
暮晨 <leisurelylicht@foxmail.com>
2015-03-14 18:20:41 +0800

    First！

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+		<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+</head>
+<body>
+
+<form action="search.php" method="post">
+IP: <input type="text" name="ipAddress"><br>
+<input type="submit" value="查询">
+</form>
+
+</body>
+</html>
