3effe5263e9d5e8425dfda5445a11268de9e2abd
暮晨 <leisurelylicht@foxmail.com>
2015-03-14 18:50:26 +0800

    样式修复

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <html>
 <head>
 		<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+		<link href="css/common.css"  type="text/css" rel="stylesheet">
 </head>
 <body>
 
-<form action="search.php" method="post">
-IP: <input type="text" name="ipAddress"><br>
-<input type="submit" value="查询">
-</form>
-
+<div class="middle">
+    <h1 align="center">IPCollection</h1>
+    <div class="middle_center">
+        <form action="search.php" method="post">
+            IP: <input type="text" name="ipAddress"><br>
+            <input type="submit" value="查询">
+        </form>
+    </div>
+</div>
 </body>
 </html>
--- search.php
+++ search.php
@@ -2,58 +2,63 @@
 <html>
 	<head>
 		<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+		<link href="css/common.css"  type="text/css" rel="stylesheet">
 	</head>
 
 <body>
 
-
-<h1 align="center">IPCollection</h1>
-
-<?php
-	echo  "Database connecting..."."<br><br>";
-	$con = mysql_connect("127.0.0.1","root","root");
-	if (!$con)
-	{
-		die('Could not connect: ' . mysql_error())."<br>";
-	}
-	else
-	{
-		echo "DataBase connect success..."."<br><br><br>";
-		
-	}
-	$ip_address = $_POST["ipAddress"];
-	mysql_query("set names 'utf8'");
-	mysql_select_db("ipsearch_db", $con);
-    $select = "SELECT * FROM ipaddress WHERE ip='".$ip_address."'";
+<div class="middle">
     
-    
-	$result = mysql_query($select);
-	echo var_dump($result);
-	
-	echo "<table border='1'>
-	<tr>
-	<th>IP </th>
-	<th>Message </th>
-	<th>RecordTime </th>
-	<th>UpdateTime </th>
-	</tr>";
-	
-	while($row = mysql_fetch_array($result))
-	  {
-	  echo "<tr>";
-	  echo "<td>" . $row['ip'] . "</td>";
-	  echo "<td>" . $row['message'] . "</td>";
-	  echo "<td>" . $row['recordtime'] . "</td>";
-	  echo "<td>" . $row['updatetime'] . "</td>";
-	  echo "</tr>";
-	  }
-	  
-	echo "</table>";
-	mysql_close($con);
-?>
+    <div class="middle_center">
+        <h1>IPCollection</h1>
+        <?php
+        	echo  "Database connecting..."."<br><br>";
+        	$con = mysql_connect("127.0.0.1","root","root");
+        	if (!$con)
+        	{
+        		die('Could not connect: ' . mysql_error())."<br>";
+        	}
+        	else
+        	{
+        		echo "DataBase connect success..."."<br><br><br>";
+        		
+        	}
+        	$ip_address = $_POST["ipAddress"];
+        	mysql_query("set names 'utf8'");
+        	mysql_select_db("ipsearch_db", $con);
+            $select = "SELECT * FROM ipaddress WHERE ip='".$ip_address."'";
+            
+            
+        	$result = mysql_query($select);
+        	echo var_dump($result);
+        	
+        	echo "<table border='1'>
+        	<tr>
+        	<th>IP </th>
+        	<th>Message </th>
+        	<th>RecordTime </th>
+        	<th>UpdateTime </th>
+        	</tr>";
+        	
+        	while($row = mysql_fetch_array($result))
+        	  {
+        	  echo "<tr>";
+        	  echo "<td>" . $row['ip'] . "</td>";
+        	  echo "<td>" . $row['message'] . "</td>";
+        	  echo "<td>" . $row['recordtime'] . "</td>";
+        	  echo "<td>" . $row['updatetime'] . "</td>";
+        	  echo "</tr>";
+        	  }
+        	  
+        	echo "</table>";
+        	mysql_close($con);
+        ?>
+   
+        <?php
+            echo "今天是 " . date("Y.m.d") . " " . date("l")."<br>";
+        ?>
 
-<?php
-echo "今天是 " . date("Y.m.d") . " " . date("l")."<br>";
-?>
+    </div>
+</div>
 </body>
 </html>
